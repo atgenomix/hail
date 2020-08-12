34bdfe7abe59d49fe836b9cf2fccfa010175ccab
Yuting LIN <johnny5584767@gmail.com>
2020-08-12 20:05:23 +0800

    ht_write, mt_write put in __init__.

--- hail/python/hail/atgenomix/__init__.py
+++ hail/python/hail/atgenomix/__init__.py
@@ -1,6 +1,8 @@
-from .datasets import execute_sql, list_datasets, import_vcf, read_matrix_table
+from .datasets import execute_sql, list_datasets, import_vcf, read_matrix_table, mt_write, ht_write
 
 __all__ = ['execute_sql',
            'list_datasets',
            'import_vcf',
-           'read_matrix_table']
+           'read_matrix_table',
+           'mt_write',
+           'ht_write']
