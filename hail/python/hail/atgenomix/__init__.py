acbd4e9cb9b8a09fe4ec0ed1407b52c34c53e678
Yuting LIN <johnny5584767@gmail.com>
2020-08-31 06:04:36 +0200

    update hail.atgenomix function name.

--- hail/python/hail/atgenomix/__init__.py
+++ hail/python/hail/atgenomix/__init__.py
@@ -1,11 +1,11 @@
-from .datasets import execute_sql, list_datasets, import_vcf, read_matrix_table, mt_write, ht_write
+from .datasets import execute_sql, list_datasets, import_vcf, read_matrix_table, write_matrix_table, write_hail_table
 from .demo import load_sample_mt, load_sample_vcf
 
 __all__ = ['execute_sql',
            'list_datasets',
            'import_vcf',
            'read_matrix_table',
-           'mt_write',
-           'ht_write',
+           'write_matrix_table',
+           'write_hail_table',
            'load_sample_mt',
            'load_sample_vcf']
